171ef3d6905c4d8f76f8a4e4d331c818b3a57c7f
RenaudC11 <renaud.claie@free.fr>
2025-08-28 12:49:30 +0200

    Delete custom_components directory

--- custom_components/const.py
+++
@@ -1,8 +0,0 @@
-DOMAIN = "mameteo"
-CONF_API_KEY = "api_key"
-CONF_STATION = "station"
-CONF_UPDATE_INTERVAL = "update_interval"  # intervalle en minutes
-CONF_ENTITY_NAME = "entity_name"          # nom de l'entité
-DEFAULT_STATION = "74182001"              # Annecy Meythet
-DEFAULT_UPDATE_INTERVAL = 6               # 6 minutes par défaut
-DEFAULT_ENTITY_NAME = "mameteo"           # valeur par défaut
