772b50dd36a2ebfc4e8111ca3ab20b42dcc3f577
RenaudC11 <renaud.claie@free.fr>
2025-08-28 20:12:07 +0200

    Update const.py

--- custom_components/mameteo/const.py
+++ custom_components/mameteo/const.py
@@ -1,8 +1,10 @@
 DOMAIN = "mameteo"
+
 CONF_API_KEY = "api_key"
 CONF_STATION = "station"
 CONF_UPDATE_INTERVAL = "update_interval"  # intervalle en minutes
 CONF_ENTITY_NAME = "entity_name"          # nom de l'entité
-DEFAULT_STATION = "74182001"              # Annecy Meythet
-DEFAULT_UPDATE_INTERVAL = 6               # 6 minutes par défaut
-DEFAULT_ENTITY_NAME = "mameteo"           # valeur par défaut
+
+DEFAULT_STATION = "69019001"              # Bron
+DEFAULT_UPDATE_INTERVAL = 6               # minutes
+DEFAULT_ENTITY_NAME = "mameteo"           # nom par défaut
